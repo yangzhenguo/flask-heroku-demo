7dd0d7a776ab21d2dc2c0917a57932ae3dc46918
Sam <sam@micous.com>
2025-09-16 17:08:27 +0800

    fibonacci

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+import time
+from functools import lru_cache
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.get('/')
+def index() -> str:
+    return render_template('index.html', time=None, n=100)
+
+@app.post('/')
+def index_post() -> str:
+    n = request.form.get('n', 100)
+    n = int(n)
+    if n < 1 or n > 100:
+        return render_template('index.html', time=None, n=n, error="Invalid input")
+    start = time.perf_counter_ns()
+    num = fib(n)
+    end = time.perf_counter_ns()
+    return render_template('index.html', num=num, time=end - start, n=n)
+
+@lru_cache(maxsize=100)
+def fib(n: int) -> int:
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
 
 def main():
     app.run(debug=True)
